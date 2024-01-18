4761694bb1baac435b3b03e683c201905c3ea1ff
shimmp <s21c2009@bene.fit.ac.jp>
2024-01-18 23:11:20 +0900

    メモの削除機能とダッシュボード画面変更

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,12 +8,18 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            </div>
+            <div>
+                <a href="/memos">
+                <div class="absolute left-10 top-1/4 h-1/4 w-2/5 bg-zinc-100 shadow-lg shadow-slate-800">
+                <p class="text-4xl absolute top-1/3 right-1/3 h-1/1 w-1/1 ">作成したメモを確認する</p>
                 </div>
-                <a href="/dashboard/search">探す</a>
-                <a href="/memos">メモ</a>
-                <a href="/meme">memememe</a>
+              </a>
+              <a href="/dashboard/search">
+                <div class="absolute top-1/4 right-10 h-1/4 w-2/5 bg-zinc-100 shadow-lg shadow-slate-800">
+                <p class="text-4xl absolute top-1/3 right-1/3 h-1/1 w-1/1">メモをする動画を探す</p>
+                </div>
+              </a>
             </div>
         </div>
     </div>
--- resources/views/memo/memo_index.blade.php
+++ resources/views/memo/memo_index.blade.php
@@ -1,24 +1,34 @@
 <x-app-layout>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Posts</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
+    <x-slot name='head'>
+        
+        
+    </x-slot>
+    
         <div class = "memos">
             @foreach ($memos as $memo)
-                <div class = 'memo'>
-                    <div class = 'root'><a href="/memos/{{$memo->id}}">{{$memo -> title}}</div>
-                    <p class = 'body' >{{$memo->body}}</p>
-                    <div class = 'edit'><a href = "/memos/{{$memo->id}}/edit">編集</div>
+                <div class='memo'>
+                    <div class='root'><a href="/memos/{{$memo->id}}">{{$memo -> title}}</a></div>
+                    <p class='body' >{{$memo->body}}</p>
+                    <div class='edit'><a href="/memos/{{$memo->id}}/edit">編集</a></div>
+                    <div>
+                        <form action="/memos/{{$memo->id}}" id="form_{{$memo->id}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class='text-slate-500'  type="button" onclick="deleteMemo({{$memo->id}})">削除</button>
+                        </form>
+                    </div>
                     -------------------------------------------------------------------
                 </div>
             @endforeach
             <div class = edit><a href ="/memos/create">作成</div>
             </div>
-    </body>
-</html>
+            <script>
+            function deleteMemo(id){
+                'use strict'
+                
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
 </x-app-layout>
